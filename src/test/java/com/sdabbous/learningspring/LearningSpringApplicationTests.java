98e3e68db8d7ef617756157b85c3ad65a21f01be
Sammy D <mobilvodoo@googlemail.com>
2023-12-08 01:09:57 +0000

    Add postgres to the Spring application

--- src/test/java/com/sdabbous/learningspring/LearningSpringApplicationTests.java
+++ src/test/java/com/sdabbous/learningspring/LearningSpringApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sdabbous.learningspring;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LearningSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sdabbous.learningspring;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LearningSpringApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
