177c8632cdc397098f01e4080df4a8cbd2e6d652
Sammy D <mobilvodoo@googlemail.com>
2023-12-11 00:49:59 +0000

    Add RoomReservationsController.java. Update index.html.

---
+++ src/main/java/com/sdabbous/learningspring/web/RoomReservationsController.java
@@ -0,0 +1,35 @@
+package com.sdabbous.learningspring.web;
+
+import com.sdabbous.learningspring.business.ReservationService;
+import com.sdabbous.learningspring.business.RoomReservation;
+import com.sdabbous.learningspring.util.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationsController {
+
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public RoomReservationsController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getReservations(@RequestParam(value = "date", required = false) String dateString, Model model){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservarvations", roomReservations);
+        return "roomres";
+
+    }
+}
